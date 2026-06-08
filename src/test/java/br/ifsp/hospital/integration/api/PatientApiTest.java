981111cbc36436febf797b31eaa8a2a767dcedb7
brenonlps <brenolopes.xy@gmail.com>
2026-06-07 22:22:06 -0300

    test: deve criar paciente com insuranceType NONE e retornar 201

--- src/test/java/br/ifsp/hospital/integration/api/PatientApiTest.java
+++ src/test/java/br/ifsp/hospital/integration/api/PatientApiTest.java
@@ -87,6 +87,21 @@ class PatientApiTest extends BaseApiIntegrationTest {
                     .statusCode(201)
                     .body("insuranceType", equalTo("PREMIUM"));
         }
+
+        @Test
+        @DisplayName("Deve criar paciente com insuranceType NONE e retornar 201")
+        void shouldCreatePatientWithNoneInsuranceAndReturn201() {
+            withAuth()
+                    .body(Map.of(
+                            "name", faker.name().fullName(),
+                            "document", faker.numerify("###.###.###-##"),
+                            "insuranceType", "NONE"
+                    ))
+                    .post("/api/v1/patients")
+                    .then()
+                    .statusCode(201)
+                    .body("insuranceType", equalTo("NONE"));
+        }
     }
 
     @Nested
